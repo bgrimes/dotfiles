5f436bbd7de6d239df1e8ef87e25e0b0e3d55313
Anthony Marion <asuahsahua@gmail.com>
2012-09-12 05:27:58 -0400

    Make symbolic link to gitconfig-common from bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,9 +4,9 @@ BACKUPFOLDER=~/dotfiles-backup-`date +%s`
 
 git pull
 function doIt() {
-	dotfiles=`ls -a | grep -v -e"^\.$" -e"^\.\.$" -e"\.git" -e"README\.md" -e"bootstrap\.sh" -e"\.DS_Store"`
+	dotfiles=`ls -a | grep -v -e"^\.$" -e"^\.\.$" -e"^\.git$" -e"gitattributes" -e"gitignore" -e"gitmodules" -e"README\.md" -e"bootstrap\.sh" -e"\.DS_Store"`
 	mkdir -p $BACKUPFOLDER
-	echo "$dotfiles" | xargs -I {} mv -f ~/{} $BACKUPFOLDER/{}
+	echo "$dotfiles" | xargs -I {} mv -f ~/{} $BACKUPFOLDER/{} 2>/dev/null
 	echo "$dotfiles" | xargs -I {} rm -rf ~/{} 2>/dev/null
 	echo "$dotfiles" | xargs -I {} ln -f -s $LINKEDDIR/{} ~/{}
 }
