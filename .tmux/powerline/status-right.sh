3cd1c1f8e314e3a2d6a05d231fe49800a8f23a04
Anthony Marion <asuahsahua@gmail.com>
2012-10-09 16:31:17 -0400

    Remove the IP address indicators from powerline

--- .tmux/powerline/status-right.sh
+++ .tmux/powerline/status-right.sh
@@ -55,20 +55,20 @@ register_segment "battery"
 #weather+=(["separator"]="${separator_left_bold}")
 #register_segment "weather"
 
-declare -A lan_ip
-lan_ip+=(["script"]="${segments_path}/lan_ip.sh")
-lan_ip+=(["foreground"]="colour255")
-lan_ip+=(["background"]="colour24")
-lan_ip+=(["separator"]="${separator_left_bold}")
-register_segment "lan_ip"
+#declare -A lan_ip
+#lan_ip+=(["script"]="${segments_path}/lan_ip.sh")
+#lan_ip+=(["foreground"]="colour255")
+#lan_ip+=(["background"]="colour24")
+#lan_ip+=(["separator"]="${separator_left_bold}")
+#register_segment "lan_ip"
 
-declare -A wan_ip
-wan_ip+=(["script"]="${segments_path}/wan_ip.sh")
-wan_ip+=(["foreground"]="colour255")
-wan_ip+=(["background"]="colour24")
-wan_ip+=(["separator"]="${separator_left_thin}")
-wan_ip+=(["separator_fg"]="white")
-register_segment "wan_ip"
+#declare -A wan_ip
+#wan_ip+=(["script"]="${segments_path}/wan_ip.sh")
+#wan_ip+=(["foreground"]="colour255")
+#wan_ip+=(["background"]="colour24")
+#wan_ip+=(["separator"]="${separator_left_thin}")
+#wan_ip+=(["separator_fg"]="white")
+#register_segment "wan_ip"
 
 #declare -A end_space
 #end_space+=(["script"]="${segments_path}/end_space.sh")
